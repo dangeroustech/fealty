226173eeeacd8e06880995f28a11035d6ae5e6dc
Josh Jacobs <josh@joshjacobs.net>
2021-11-27 10:35:48 +0000

    ci: debugging

--- backend/mongo_accounts.go
+++ backend/mongo_accounts.go
@@ -15,6 +15,7 @@ func dbConnect() *mongo.Client {
 	MONGO_URI := os.Getenv("MONGO_URI")
 	MONGO_USER := os.Getenv("MONGO_USER")
 	MONGO_PASS := os.Getenv("MONGO_PASS")
+	log.Printf("Connecting to Mongo at %s with username %s and password %s", MONGO_URI, MONGO_USER, MONGO_PASS)
 	var ctx = context.TODO()
 	clientOptions := options.Client().ApplyURI(MONGO_URI).SetAuth(options.Credential{AuthSource: "fealty", Username: MONGO_USER, Password: MONGO_PASS})
 	client, err := mongo.Connect(ctx, clientOptions)
