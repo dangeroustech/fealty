6acf10b3c87a6f7469f49b94d9664b01bfb5fb82
Josh Jacobs <josh@joshjacobs.net>
2021-11-28 01:38:46 +0000

    ???

--- deploy/packer/backend/service_setup.sh
+++ deploy/packer/backend/service_setup.sh
@@ -5,12 +5,14 @@ apt-get -qq update
 apt-get -qq upgrade
 
 echo "###---Enabling Fealty Service---###"
+chmod +x /usr/bin/fealty
 echo "MONGO_URI=$MONGODB_FEALTY_URI" >> /etc/fealty/VARS
 echo "MONGO_USER=fealty" >> /etc/fealty/VARS
 echo "MONGO_PASS=$MONGODB_FEALTY_PASS" >> /etc/fealty/VARS
 echo "FEALTY_CONFIG=/etc/fealty" >> /etc/fealty/VARS
 systemctl enable fealty
 systemctl start fealty
+systemctl status fealty
 
 journalctl | grep fealty
 
