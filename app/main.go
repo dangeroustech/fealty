0a1eec97582cac5ad746e55e013743b9dfcf8551
Josh Jacobs <josh@joshjacobs.net>
2021-12-12 20:52:48 +0000

    feat: basic basic auth complete

--- app/main.go
+++ app/main.go
@@ -6,17 +6,12 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html"
 )
 
-func api_middleware(c *fiber.Ctx) error {
-	fmt.Printf("Request from %#v\n", c.IP()) // technically only needs %s
-
-	return c.Next()
-}
-
-func browser_middleware(c *fiber.Ctx) error {
+func middleware(c *fiber.Ctx) error {
 	fmt.Printf("Request from %#v\n", c.IP()) // technically only needs %s
 	return c.Next()
 }
@@ -32,7 +27,30 @@ func main() {
 		AppName:       "FealTY v0.5.0",
 		Views:         htmlEngine,
 	})
+	// Logging
 	app.Use(logger.New())
+	// Auth
+	app.Use(basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"john":  "doe",
+			"admin": "123456",
+		},
+		Realm: "Forbidden",
+		Authorizer: func(user, pass string) bool {
+			if user == "john" && pass == "doe" {
+				return true
+			}
+			if user == "admin" && pass == "123456" {
+				return true
+			}
+			return false
+		},
+		Unauthorized: func(c *fiber.Ctx) error {
+			return c.SendStatus(401)
+		},
+		ContextUsername: "_user",
+		ContextPassword: "_pass",
+	}))
 
 	// Root API Route
 	api := app.Group("/api") // /api
@@ -41,11 +59,11 @@ func main() {
 	v1 := api.Group("/v1") // /api/v1
 
 	// Mass Account Routes
-	v1.Get("/accounts/get", getAccounts, api_middleware)         // /api/v1/accounts
-	v1.Get("/accounts/view", renderAccounts, browser_middleware) // /api/v1/accounts/view
+	v1.Get("/accounts/get", getAccounts, middleware)     // /api/v1/accounts
+	v1.Get("/accounts/view", renderAccounts, middleware) // /api/v1/accounts/view
 
 	// Individual Account Routes
-	acc := v1.Group("/account", api_middleware) // /api/v1/account
+	acc := v1.Group("/account", middleware) // /api/v1/account
 	acc.Get("", getAccount)
 	acc.Post("", createAccount)
 	acc.Put("", updateAccount)
