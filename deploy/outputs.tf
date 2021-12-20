d4fc27e9e0309c4da649942bad1a2287c8c9f9de
Josh Jacobs <josh@joshjacobs.net>
2021-12-20 13:45:59 +0000

    feat: add tf outputs file

---
+++ deploy/outputs.tf
@@ -0,0 +1,22 @@
+output "Domain_A_Record" {
+    value = linode_domain_record.rewards.target
+    description = "Domain A Reord"
+    depends_on = [
+        linode_instance.app,
+    ]
+}
+
+output "App_Public_IP" {
+    value = linode_instance.app.ip_address
+    description = "App Instance's Public IP"
+}
+
+output "App_Private_IP" {
+    value = linode_instance.app.config[0].interface[1].ipam_address
+    description = "App Instance's Private IP"
+}
+
+output "DB_Private_IP" {
+    value = linode_instance.db.config[0].interface[0].ipam_address
+    description = "DB Instance's Private IP"
+}
