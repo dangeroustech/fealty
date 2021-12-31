cb9f43e62479c3d46ffd33f051239a23580835de
Josh Jacobs <josh@joshjacobs.net>
2021-12-31 16:28:56 +0000

    fix: tweak firewall rules

--- deploy/firewall.tf
+++ deploy/firewall.tf
@@ -4,7 +4,40 @@ resource "linode_firewall" "fealty_fw" {
   tags  = ["fealty"]
 
   inbound_policy = "DROP"
+
+  inbound {
+    label    = "drop-inbound-tcp"
+    action   = "DROP"
+    protocol = "TCP"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+  inbound {
+    label    = "drop-inbound-udp"
+    action   = "DROP"
+    protocol = "UDP"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+
+  inbound {
+    label    = "allow-inbound-icmp"
+    action   = "ACCEPT"
+    protocol = "ICMP"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+
   outbound_policy = "ACCEPT"
+
+  outbound {
+    label    = "allow-https"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "443"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
 
   linodes = [linode_instance.app[count.index].id]
 }
